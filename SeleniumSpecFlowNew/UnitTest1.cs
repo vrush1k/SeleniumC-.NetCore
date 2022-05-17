67c6f4b968906bcfa4dd6f95067da06f2ade2819
VS <dev.megaminds@gmail.com>
2022-05-17 08:26:14 +0530

    initial commit with basic

---
+++ SeleniumSpecFlowNew/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace SeleniumSpecFlowNew
+{
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            Console.WriteLine("hhh");
+            driver = new ChromeDriver(path + @"\drivers\");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("tom");
+            Console.WriteLine("hhh");
+            Assert.Pass();
+        }
+    }
+}
